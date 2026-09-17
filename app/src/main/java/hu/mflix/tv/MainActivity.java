a1328c7ab71b3731a3ea31e023dfb861b0ff548c
hunkriss <hunkriss@gmail.com>
2026-09-17 16:47:25 +0200

    Update MainActivity.java

--- app/src/main/java/hu/mflix/tv/MainActivity.java
+++ app/src/main/java/hu/mflix/tv/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -14,7 +15,7 @@ public class MainActivity extends Activity {
 
     private WebView webView;
 
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint({"SetJavaScriptEnabled", "SetWebViewClient"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,22 +25,44 @@ public class MainActivity extends Activity {
 
         WebSettings settings = webView.getSettings();
 
+        // JavaScript és webes tárhely
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setDatabaseEnabled(true);
 
+        // Videólejátszás
         settings.setMediaPlaybackRequiresUserGesture(false);
 
+        // TV / nagy képernyő
         settings.setLoadWithOverviewMode(true);
         settings.setUseWideViewPort(true);
 
+        // Gyorsabb működés
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+
+        // Hardveres gyorsítás
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
+        // Cookie-k
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
+
+        // WebView
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
 
+        // TV távirányítóhoz fókusz
         webView.setFocusable(true);
         webView.setFocusableInTouchMode(true);
         webView.requestFocus(View.FOCUS_DOWN);
 
+        // Ne legyen WebView-keret
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+
+        // MFLIX
         webView.loadUrl("https://mflix.hu");
     }
 
@@ -48,7 +71,9 @@ public class MainActivity extends Activity {
 
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
 
-            switch (event.getKeyCode()) {
+            int keyCode = event.getKeyCode();
+
+            switch (keyCode) {
 
                 case KeyEvent.KEYCODE_DPAD_UP:
                 case KeyEvent.KEYCODE_DPAD_DOWN:
@@ -57,6 +82,8 @@ public class MainActivity extends Activity {
                 case KeyEvent.KEYCODE_DPAD_CENTER:
                 case KeyEvent.KEYCODE_ENTER:
 
+                    // A távirányító gombjait továbbadjuk
+                    // a WebView / weboldal számára.
                     return super.dispatchKeyEvent(event);
             }
         }
